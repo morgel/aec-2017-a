169ca36e32779675f2d9f7f096e50763dbefc5af
Unknown <patrick.friedrich@outlook.com>
2017-06-23 10:26:42 +0200

    Auto stash before merge of "kaziller-initial" and "origin/kaziller-initial"

--- public/src/app/backer/backer.component.ts
+++ public/src/app/backer/backer.component.ts
@@ -9,7 +9,22 @@ export class BackerComponent implements OnInit {
 
   constructor() { }
 
+// return list with all projects backed by this backer
+  getMyProjects(){}
+
   ngOnInit() {
+    // projects = getMyProjects();
   }
+
+  projects=[
+    {
+      name: "io disruptor",
+      fundingStatus: 78
+    },
+    {
+      name: "Kohlekraftwerk",
+      fundingStatus: 34
+    }
+  ]
 
 }
--- public/src/app/project-owner/project-owner.component.ts
+++ public/src/app/project-owner/project-owner.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectOwnerComponent implements OnInit {
 
+selectedProject: null;
+
   constructor() { }
 
+  // return a list with all projects created by this project owner
+  getMyProjects(){}
+
+  // kill this project and Smart Contract
+  cancelProject(){}
+
   ngOnInit() {
+    // projects = getMyProjects();
   }
+
+  onSelect(project): void {
+    this.selectedProject = project;
+  }
+
+  projects=[
+    {
+      name: "Next biggy",
+      fundingStatus: 67
+    },
+    {
+      name: "io disruptor",
+      fundingStatus: 78
+    },
+    {
+      name: "Kohlekraftwerk",
+      fundingStatus: 34
+    }
+  ]
 
 }
