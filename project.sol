60efdb43883468566e3915e55a9f88593a721675
johnny <johnnytran@gmx.de>
2017-07-04 18:25:28 +0200

    was ist trauriger

--- project.sol
+++ project.sol
@@ -53,25 +53,33 @@ contract Project{
     totalWithdrawnAmount = 0;
   }
 
-function fundwithtoken() payable public{
-     if(backers[msg.sender] != 0){
-      backers[msg.sender] += msg.value;
-      tokens_of_backers[msg.sender] += msg.value/nominal_token_value;
-      emitted_tokens += msg.value/nominal_token_value;
-      paid_in += msg.value;
-    } else {
-      indicesAddresses[numberOfBackers] = msg.sender;
-      numberOfBackers += 1;
-      tokens_of_backers[msg.sender] += msg.value/nominal_token_value;
-      emitted_tokens += msg.value/nominal_token_value;
-      backers[msg.sender] += msg.value;
-      paid_in += msg.value;
+  function() payable public{
+    assert(!isFunded());
+    if(backers[msg.sender] == 0){
+        indicesAddresses[numberOfBackers] = msg.sender;
+        numberOfBackers += 1;
     }
+    
+      tokens_of_backers[msg.sender] += msg.value/nominal_token_value;
+      emitted_tokens += msg.value/nominal_token_value;
+      backers[msg.sender] += msg.value;
+      paid_in += msg.value;
 }
 
 function getmyTokens() constant returns(uint){
       return tokens_of_backers[msg.sender];
   }
+  
+function getallTokenOwners() constant returns(address[], uint[]) {
+    address[]memory addresses  = new address[](numberOfBackers);
+    uint[]memory tokens  = new uint[](numberOfBackers);
+    for(uint i = 0; i < numberOfBackers; i++){
+            addresses[i] = indicesAddresses[i];
+            tokens[i] = tokens_of_backers[indicesAddresses[i]];
+    }
+    return (addresses,tokens);
+}
+
 function getemittedtokens() constant returns(uint){
       return emitted_tokens;
   }
@@ -97,10 +105,17 @@ function gettokensoffered() constant returns(uint, uint){
 
 function buyTokens(address tokenowner,uint tokennumber) payable public{
     if (tokens_offered[tokenowner]>=tokennumber){
+    if(tokens_of_backers[msg.sender] == 0){
+        indicesAddresses[numberOfBackers] = msg.sender;
+        numberOfBackers += 1;
+    }
+        
         tokenowner.transfer(tokennumber*offered_price[tokenowner]);
         tokens_offered[tokenowner]-=tokennumber;
         tokens_of_backers[tokenowner]-=tokennumber;
         tokens_of_backers[msg.sender]+=tokennumber;
+        
+        
     }
     else
     {throw;}
@@ -108,20 +123,6 @@ function buyTokens(address tokenowner,uint tokennumber) payable public{
 
     
 
-  function() payable public{
-  assert(!isFunded());
-  else{
-    if(backers[msg.sender] != 0){
-      backers[msg.sender] += msg.value;
-      paid_in += msg.value;
-    } else {
-      indicesAddresses[numberOfBackers] = msg.sender;
-      numberOfBackers += 1;
-      backers[msg.sender] += msg.value;
-      paid_in += msg.value;
-    }
-  }
-  }
 
   function myTokenShare() constant returns(uint){
       uint tokenShareInPercent = 0;
@@ -130,6 +131,9 @@ function buyTokens(address tokenowner,uint tokennumber) payable public{
       tokenShareInPercent = backerPaidInAmount * percentOfAllTokensDistributedToBackers / paid_in ;
       return tokenShareInPercent;
   }
+  
+  
+  
 
   function isFunded() constant public returns(bool){
   if(now > fundingEnd){
@@ -157,7 +161,7 @@ function buyTokens(address tokenowner,uint tokennumber) payable public{
           var alreadyFunded = isFunded();
           if(alreadyFunded){
               if(amountToWithdraw <= (this.balance)){
-                  owner.send(amountToWithdraw);
+                  owner.transfer(amountToWithdraw);
                   totalWithdrawnAmount+=amountToWithdraw;
                   return true;
               }
@@ -171,7 +175,7 @@ function buyTokens(address tokenowner,uint tokennumber) payable public{
     //redistribute funds
         for(uint i = 0; i < numberOfBackers; i++){
             address backer = indicesAddresses[i];
-            backer.send(backers[backer]);
+            backer.transfer(backers[backer]);
         }
     selfdestruct(owner); //should return 0 value
     }
@@ -208,95 +212,5 @@ function buyTokens(address tokenowner,uint tokennumber) payable public{
   }
   function getFundingEnd() constant public returns(uint){
     return fundingEnd;
-  }
-
-  struct Offer {
-    address seller;
-    uint price;
-    uint created;
-    uint offerDeadline;
-    uint numberOfUnits;
-    address buyer;
-    uint id;
-  }
-
-  uint counterOffers = 0;
-  mapping(uint => Offer) idOffer;
-  Offer[] currentOffers;
-  Offer[] archivedOffers;
-
-  function createOffer(uint numberOfUnitsToTrade, uint specifiedPrice){
-    assert(thisSellerhasOnlyOneCurrentOffer());
-    assert(backers[msg.sender] >= numberOfUnitsToTrade);
-
-    Offer newOffer;
-    newOffer.seller = msg.sender;
-    newOffer.price = specifiedPrice;
-    newOffer.created = now;
-    newOffer.offerDeadline = created + 5 * 1 days;
-    newOffer.numberOfUnits = numberOfUnitsToTrade;
-    newOffer.buyer = 0;
-    newOffer.id = counterOffers;
-
-    idOffer[counterOffers] = newOffer;
-    currentOffers.push(newOffer);
-
-    counterOffers += 1;
-
-  }
-
-  function thisSellerhasOnlyOneCurrentOffer() constant private returns(bool){
-    hasOnlyOneOffer = true;
-    for(uint i = 0; i < currentOffers.length; i++){
-        if(msg.sender == currentOffers[i].seller){
-          hasOnlyOneOffer = false;
-          return hasOnlyOneOffer;
-        }
-    }
-    return hasOnlyOneOffer;
-  }
-
-  function buyShares(uint offerId){
-    // like this the offer's Id is the parameter to identify the offer
-    // other parameters would be possible, e.g. seller address
-    Offer offerToBuy;
-    for(uint i = 0; i < currentOffers.length; i++){
-        if(offerId == currentOffers[i].id){
-          offerToBuy = currentOffers[i];
-        }
-    }
-    assert(offerToBuy != 0);
-    assert(offerToBuy.buyer == 0);
-    assert(msg.value >= offerToBuy.price);
-
-    offerToBuy.buyer = msg.sender;
-    (offerToBuy.seller).transfer(msg.value);
-
-    backers[offerToBuy.seller] -= offerToBuy.numberOfUnits;
-    if(backers[msg.sender] != 0){
-      backers[msg.sender] += offerToBuy.numberOfUnits;
-    } else {
-      indicesAddresses[numberOfBackers] = msg.sender;
-      numberOfBackers += 1;
-      backers[msg.sender] += offerToBuy.numberOfUnits;
-    }
-
-    removeOfferfromCurrentOffers(offerToBuy);
-    archivedOffers.push(offerToBuy);
-  }
-
-  function removeOfferfromCurrentOffers(Offer offerToDelete){
-    uint idToFind = offerToDelete.id;
-    uint indexOfOffer = -1;
-    for(uint i = 0; i < currentOffers.length; i++){
-        if(idToFind == currentOffers[i].id){
-          indexOfOffer = i;
-        }
-    }
-    assert(indexOfOffer != -1);
-
-    //swap with last, so gaps are prevented
-    currentOffers[indexOfOffer] = currentOffers[currentOffers.length-1];
-    delete currentOffers[currentOffers.length-1];
   }
 }
